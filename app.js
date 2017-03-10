b2c2732dea29ee6752b2044e146d188a799fa263
Nathan Anecone <nanecone.gmail.com>
2017-03-10 18:28:46 -0500

    initialized project

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var bodyParser = require('body-parser');
-
+var models = require('./models');
 // templating boilerplate setup
 app.engine('html', nunjucks.render); // how to render html templates
 app.set('view engine', 'html'); // what file extension do our templates have
@@ -26,7 +26,25 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/', routes);
 
 // start the server
-app.listen(1337, function(){
+
+Page.sync();
+User.sync()
+
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    server.listen(3000, function () {
+        console.log('Server is listening on port 3001!');
+    });
+})
+.catch(console.error);
+
+//Not sure if ^^^ we should wrap the listener on line 47 in this .sync or if server should be named app
+
+app.listen(3000, function(){
   console.log('listening on port 1337');
 });
 

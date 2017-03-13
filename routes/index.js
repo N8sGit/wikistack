e7ad610bb6b1135695dbe7f2db9e1fefde852738
Wilson Wong <wilsonwong13@gmail.com>
2017-03-13 09:42:55 -0400

    message

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
-const express = require('express')
-const app = express();
-const morgan = require('morgan')
+const express = require('express');
+const router = express.Router();
+// could use one line instead: const router = require('express').Router();
+//const tweetBank = require('../tweetBank');
+const client = require('../db/index.js');

ca9f55bc98aa117094a099fa299731af0747d929
Wilson Wong <wilsonwong13@gmail.com>
2017-03-13 14:07:08 -0400

    chages

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistacks');
+var db = new Sequelize('postgres://localhost:5432/wikistack');
 
 
 var Page = db.define('page', {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,3 @@
-
-const express = require('express');
-const router = express.Router();
-// could use one line instead: const router = require('express').Router();
-//const tweetBank = require('../tweetBank');
-const client = require('../db/index.js');
-
 const express = require('express')
 const morgan = require('morgan')
 const wikiRouter = require('./wiki');
